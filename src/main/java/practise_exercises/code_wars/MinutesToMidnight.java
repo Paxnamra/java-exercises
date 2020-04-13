92949d3f633e4648b4ed42edad05eba0e014a217
mblizniuk <bialyfeniks4@gmail.com>
2020-04-13 02:45:30 +0200

    Code Wars - Minutes to midnight || refactoring

--- src/main/java/practise_exercises/code_wars/MinutesToMidnight.java
+++ src/main/java/practise_exercises/code_wars/MinutesToMidnight.java
@@ -8,15 +8,11 @@ public class MinutesToMidnight {
     public static String countMinutes(Date d) {
         LocalDateTime convertDate = d.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
 
-        long hoursToMinutes = convertDate.toLocalTime().getHour() * 60;
-        long secondsToMinutes = convertDate.toLocalTime().getSecond() % 1000;
+        int hours = convertDate.toLocalTime().getHour() * 60;
+        int seconds = convertDate.toLocalTime().getSecond() % 1000;
+        int countMinutes = 1440 - (hours + convertDate.getMinute() + (seconds > 0 ? 1 : 0));
 
-        if (secondsToMinutes % 60 > 0) {
-            secondsToMinutes = 1;
-        }
-
-        long minutes = hoursToMinutes + convertDate.getMinute() + secondsToMinutes;
-        return ((24 * 60 - minutes) == 1) ? 24 * 60 - minutes + " minute" : 24 * 60 - minutes + " minutes";
+        return countMinutes + (countMinutes == 1 ? " minute" : " minutes");
     }
 }
 

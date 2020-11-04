efecbe1e5c1511fcb6dd2df1127437af90e13fe4
Paxnamra <bialyfeniks4@gmail.com>
2020-11-04 03:45:22 +0100

    codewars: split strings - refactored solution

--- src/main/java/practise_exercises/code_wars/StringSplit.java
+++ src/main/java/practise_exercises/code_wars/StringSplit.java
@@ -4,21 +4,19 @@ public class StringSplit {
     public static String[] solution(String s) {
         s = (s.length() % 2 == 0) ? s : s + "_";
         String[] charPairs = new String[s.length() / 2];
-        int arrIndex = 0;
+        int k = 0;
 
         for (int i = 0, j = 0; i < s.length(); i += 2) {
-            j += 2;
+            j = (j > s.length()) ? j - 1 : j + 2;
 
-            if (j > s.length()) {
-                j -= 1;
-            }
-
-            charPairs[arrIndex] = s.substring(i, j);
-            arrIndex++;
+            charPairs[k] = s.substring(i, j);
+            k++;
         }
 
         return charPairs;
     }
 }
+
+
 
 

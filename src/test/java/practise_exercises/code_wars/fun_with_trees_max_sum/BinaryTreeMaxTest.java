579d19e73d1e96a37ef6d1e81e18e54241096889
Paxnamra <bialyfeniks4@gmail.com>
2020-12-15 23:18:12 +0100

    added test for preorder tree traversal void method print out

--- src/test/java/practise_exercises/code_wars/fun_with_trees_max_sum/BinaryTreeMaxTest.java
+++ src/test/java/practise_exercises/code_wars/fun_with_trees_max_sum/BinaryTreeMaxTest.java
@@ -1,13 +1,19 @@
 package practise_exercises.code_wars.fun_with_trees_max_sum;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.*;
-import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class BinaryTreeMaxTest {
     private TreeNode node;
+    private final PrintStream systemOuptut = System.out;
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
     @Before
     public void setup() {
@@ -16,10 +22,18 @@ public class BinaryTreeMaxTest {
         node.right = new TreeNode(2);
         node.left.left = new TreeNode(3);
         node.left.right = new TreeNode(4);
+
+        System.setOut(new PrintStream(outputStream));
     }
 
     @Test
     public void shouldBinaryTreeMax_traverseTree_AndReturnMaxNodesSum() {
         assertThat(BinaryTreeMax.maxSum(node), is(5));
+    }
+
+    @Test
+    public void shouldBinaryTreeMax_postorderTraverseTree_AndReturnVisitedNodesValues() {
+        BinaryTreeMax.postorder(node);
+        Assert.assertEquals("3 4 1 2 0", outputStream.toString().trim());
     }
 }
